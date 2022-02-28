8aeadda721cbdc29ccf3afc9ac1ab11537b6bc9d
CKarls1 <cka048@uit.no>
2022-02-28 12:48:24 +0100

    Oppdatering
    Oppdatering

--- Mappeoppgave 4/Code_oppgave_4.R
+++ Mappeoppgave 4/Code_oppgave_4.R
@@ -4,36 +4,45 @@ library(tidyverse)
 library(rvest)
 library(rlist)
 
+# samarbeidet med Rudi Hansen, Marcus Edvardsen og Yves Sebazungu
+# alle leverer individuelle besvarelser 
 
-
-# insperert av 
+# inspirert av 
 # https://datacornering.com/use-data-frame-row-as-a-column-names-in-r/
+# utgitte forelesninger i kurset
 
 
 
-scrape <- function(url, Emnekode) {
+#lager en liste med alle fagene vi har. SOK-1005, BED-2021, BED-2032
+# benytter de fag kodene jeg selv har da jeg har tre valgfag og ikke samme kursplan.
+Timeplan_URL <- list("https://timeplan.uit.no/emne_timeplan.php?sem=22v&module%5B%5D=SOK-1005-1&week=1-20&View=list", 
+                     "https://timeplan.uit.no/emne_timeplan.php?sem=22v&module%5B%5D=BED-2032-1&View=list",
+                     "https://timeplan.uit.no/emne_timeplan.php?sem=22v&module%5B%5D=BED-2021-1&View=list")
+
+
+
+
+
+
+Skrape_func <- function(url) {
   return(read_html(url) %>% 
            html_nodes(.,'table') %>% 
            html_table(., fill = TRUE) %>% 
-           list.stack(.) %>% 
-           janitor::row_to_names(., 1) %>% 
+           list.stack(.) %>% # slår sammen listene til et datasett
+           janitor::row_to_names(., 1) %>% # setter første rad som header
            separate(Dato,
                     into = c("Dag", "Dato"),
-                    sep = "(?<=[A-Za-z])(?=[0-9])") %>% 
+                    sep = "(?<=[A-Za-z])(?=[0-9])") %>% # separerer dag og dato
            .[-length(.$Dag),] %>%
            filter(!str_detect(Dato, "Dato"), 
-                  !str_detect(Beskrivelse, "international|(WISEFLOW)| Alta")) %>% 
-           zoo::na.locf(.) %>% 
+                  !str_detect(Beskrivelse, "international|(WISEFLOW)| Alta")) %>% # filtrerer de variablene jeg ikke ønsker 
+           zoo::na.locf(.) %>%  # Fyller inn NA med veridien over, da enkelte emner har samme dato på to forskjellige steder
            mutate(Dato=as.Date(Dato, format = "%d.%m.%Y"), 
                   Uke = strftime(Dato, format = "%V"), 
-                  Dag = strftime(Dato, format = "%A") )%>% 
-           select(Dag,Dato,Uke,Tid,Rom, Lærer,Beskrivelse))
+                  Dag = strftime(Dato, format = "%A") )%>% # lager en ny variabel for dag så de tomme dataene blir fylt med riktig dag
+           select(Dag,Dato,Uke,Tid,Rom, Lærer)) # velger det som skal inn i listen
 }
 
 
-SOK_1005 <- scrape("https://timeplan.uit.no/emne_timeplan.php?sem=22v&module%5B%5D=SOK-1005-1&week=1-20&View=list", "SOK-1005")
-BED_2032 <- scrape("https://timeplan.uit.no/emne_timeplan.php?sem=22v&module%5B%5D=BED-2032-1&View=list", "BED-2031")
-BED_2021 <- scrape("https://timeplan.uit.no/emne_timeplan.php?sem=22v&module%5B%5D=BED-2021-1&View=list", "BED-2021")
-
-timeplan <- bind_rows(SOK_1005, BED_2032, BED_2021)
-
+timeplan <- map(Timeplan_URL, Skrape_func) 
+timeplan
